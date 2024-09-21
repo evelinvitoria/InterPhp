e7863459f87fa6b9badb8f4d0ad59a542126bcf6
Kayo <114114263+kayozer@users.noreply.github.com>
2024-09-21 11:31:03 -0300

    Add files via upload

---
+++ salvarUsuario.php
@@ -0,0 +1,18 @@
+<?php 
+switch ($_REQUEST['acao']) {
+	case 'cadastrar':
+		$nome = $POST["nome"];
+		$email = $POST["email"];
+		$data_nasc = $POST["data_nasc"];
+
+		$sql = "INSERT INTO usuario (nome, email, senha, data_nasc) VALUES( '{$nome}', '{email}', '{senha}', '{data_nasc'};
+		)
+				break;
+
+				$res = $conn->query($sql);	
+	case 'editar':
+	break;
+
+	case 'excluir'
+	break;
+}
